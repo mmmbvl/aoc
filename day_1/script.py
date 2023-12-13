cc439910f3dca44c12e9220bce209e5816ab1672
mmmbvl <mmmbvl@gmail.com>
2023-12-12 21:43:56 -0800

    d1p1 correction

--- day_1/script.py
+++ day_1/script.py
@@ -2,15 +2,18 @@ inp = open('input').readlines()
 
 tot = 0
 for line in inp:
+    print("Processing line:")
+    print(line)
     lt = 0
     for a in line:
         if (a.isdigit()):
             lt = lt + (int(a) * 10)
-    for a in line:
-        b = 0
+            print("Found first digit as ", a)
+            break
+    for a in reversed(line):
         if (a.isdigit()):
-            lt = lt + int(a)
-            lt = lt - int(b)
-            b = int(a)
+            lt = lt + (int(a))
+            print("Found last digit as ", a)
+            break
     tot = lt + tot
 print(tot)
