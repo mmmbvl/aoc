adee6fc20c3f43dd95ae423d84875a846f00ce53
mmmbvl <mmmbvl@gmail.com>
2023-12-27 09:54:28 -0800

    D11P2 solution

--- day_11/script.py
+++ day_11/script.py
@@ -72,17 +72,60 @@ print("P1: ", total)
 
 
 
+
+
+def discover_empty_rows(m):
+    emptyRows = []
+    for r in range(len(m)):
+        isEmptyRow = True
+        for c in range(len(m[r])):
+            if m[r][c] != ".":
+                isEmptyRow = False
+        if isEmptyRow:
+            emptyRows.append(r)
+    return emptyRows
+
+def discover_empty_cols(m):
+    emptyCols = []
+    for c in range(len(m[0])):
+        isEmptyCol = True
+        for r in range(len(m)):
+            if m[r][c] != ".":
+                isEmptyCol = False
+        if isEmptyCol:
+            emptyCols.append(c)
+    return emptyCols
+
+def compensated_dist_btwn_galaxies(gs, i, j, e_rows, e_cols):
+    a = gs[i][0] - gs[j][0] if (gs[i][0] > gs[j][0]) else gs[j][0] - gs[i][0]
+    b = gs[i][1] - gs[j][1] if (gs[i][1] > gs[j][1]) else gs[j][1] - gs[i][1]
+
+    for e_row in e_rows:
+        if (gs[i][0] < e_row and gs[j][0] > e_row) or (gs[i][0] > e_row and gs[j][0] < e_row):
+            a += 1000000 - 1
+
+    
+    for e_col in e_cols:
+        if (gs[i][1] < e_col and gs[j][1] > e_col) or (gs[i][1] > e_col and gs[j][1] < e_col):
+            b += 1000000 - 1
+
+    return a + b
+
 g_map = make_map(inp)
 # print("\n".join(["".join(x) for x in g_map]))
-e_map = expand_map(g_map, 1000000 - 1)
+e_rows = discover_empty_rows(g_map)
+e_cols = discover_empty_cols(g_map)
+
+print(e_rows)
 # print("\n".join(["".join(x) for x in e_map]))
-galaxies = grab_galaxies(e_map)
-print(galaxies)
+galaxies = grab_galaxies(g_map)
+# print(galaxies)
+
 
 total = 0
 for (i, x) in enumerate(galaxies):
     for (j,y) in enumerate(galaxies):
         if (j > i):
-            total += dist_btwn_galaxies(galaxies, i, j)
+            total += compensated_dist_btwn_galaxies(galaxies, i, j, e_rows, e_cols)
 
 print("P2: ", total)
