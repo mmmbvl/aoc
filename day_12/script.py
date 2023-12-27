883d6f7520e277726081ebad090842da81865261
mmmbvl <mmmbvl@gmail.com>
2023-12-27 15:24:37 -0800

    D12P1 solution

--- day_12/script.py
+++ day_12/script.py
@@ -1,101 +1,81 @@
 inp = open("input").readlines()
 
-marker_data = []
+symbol_data = []
 number_data = []
 
 for line in inp:
     a = line.split(" ")
-    marker_data.append(list(a[0]))
+    symbol_data.append(list(a[0]))
     number_data.append(list(map(lambda x : int(x), a[1].split(","))))
 
-# print(marker_data, number_data)
+def attempt_placement(symbols, last_placed, sz):
+    valid_placements = []
+    print("Attempting placement into: ")
+    print(symbols)
+    
+    # dbg_arr = ["." for x in range(len(symbols) + sz + 1)]
+    terminal = False
 
-def validate(symbols, numbers):
-    print("Validating: ", symbols, "\n>Desired:                        ", numbers)
-    g = []
-    active = False
-    count = 0
-    for s in symbols:
-        if s == "#" and active == False:
-            active = True
-            count += 1
-        elif s == "#" and active == True:
-            count += 1
-        elif s == "." and active == True:
-            active = False
-            g.append(count)
-            count = 0
-        elif s == "." and active == False:
+    for i in range(len(symbols)):
+        if i < last_placed:
             continue
-        else:
-            return False
+        dbg_arr = ["." for x in range(len(symbols) + sz + 1)]
+        dbg_arr[i] = "^"
+        dbg_arr[i + sz + 1] = "^"
+        
+        valid = True
+        if symbols[i] == "#":
+            dbg_arr[i] = ":"
+            terminal = True
+            valid = False
+            break
+        for a in range(sz):
+            if (i + a + 1 >= len(symbols)) or symbols[i + a + 1] == ".":
+                dbg_arr[i + a + 1] = "X"
+                valid = False
+            else:
+                dbg_arr[i + a + 1] = "O"
+        if (i + sz + 1 < len(symbols)) and symbols[i + sz + 1] == "#":
+            dbg_arr[i + sz + 1] = ":"
+            valid = False
+        
+        print(dbg_arr, " ------> ", valid)
+
+        if valid:
+            valid_placements.append(i + sz + 1)
     
-    print("Measured:                        ", g)
+    print("                                                 Terminal is: ", terminal)
+    print("Result: ", valid_placements)
+    return valid_placements
 
-    if len(g) != len(numbers):
-        return False
-    
-    for i, n in enumerate(numbers):
-        if g[i] != numbers[i]:
-            return False
-    
-    return True
+def place_all(symbs, numbs):
+    print("---place_all:            ", symbs, numbs)
+    symbs.insert(0,".")
+    print("---place_all:   prefix   ", symbs, numbs)
+    valid_stk = [0]
+    for i,n in enumerate(numbs):
+        next_stage_stacks = []
+        for lp in valid_stk:
+            next_stage_stacks += attempt_placement(symbs, lp, n)       
+        valid_stk = next_stage_stacks
 
-def get_unknowns(symbols):
-    unk = []
-    for i,s in enumerate(symbols):
-        if s == "?":
-            unk.append(i)
-    return unk
+    print("Cleaning up: ", valid_stk)
+    removes = set()
+    for i,n in enumerate(valid_stk):
+        for j,z in enumerate(symbs):
+            if j > n and symbs[j] == "#":
+                removes.add(i)
+    print("Following are invalid: ", list(removes))
+    for k in reversed(list(removes)):
+        valid_stk.pop(k)
+    print("Cleaned up: ", valid_stk)
 
-def possible_comb(unk_sym):
-    print("545222-----------")
-    pile = []
-    pile.append(unk_sym)
-    for i,x in enumerate(unk_sym):
-        t = unk_sym[:]
-        t[i] = "#"
-        prefix = t[0:i+1]
-        suffix = t[i+1:]
-        # print("prefix:",prefix,"suffix:",suffix)
-        new_suffixes = possible_comb(suffix)
-        for new_suffix in new_suffixes:
-            new_t = prefix + new_suffix
-            pile.append(new_t)
-    return pile
-# lkp = {}
-# for i in range(25):
-#     lkp[i] = possible_comb(["." for c in range(i)])
-
-def num_possible_combinations(symbols, numbers):
-    unk = get_unknowns(symbols)
-    total_valid = 0
-    pwk = symbols[:]
-
-    blank = []
-    for e in unk:
-        blank.append(".")
-    
-    all_filler_possibles = possible_comb(blank)
-    # all_filler_possibles = lkp[len(blank)]
-
-    # for n in all_filler_possibles:
-    #     print(n)
-
-    for p, filler_possible in enumerate(all_filler_possibles):
-        for i,fp in enumerate(filler_possible):
-            pwk[unk[i]] = fp
-        print("\n\nFilling in: ", symbols, "( ",p, " of ",len(all_filler_possibles)," )")
-        if validate(pwk, numbers):
-            print("========> Valid")
-            total_valid += 1
-        else:
-            print("========> Invalid")
-    
-    return total_valid
+    print("---place_all:   result   ", len(valid_stk), symbs, numbs)
+    return len(valid_stk)
 
 total = 0
-for i, s in enumerate(marker_data):
-    total += num_possible_combinations(marker_data[i], number_data[i])
+for i in range(len(symbol_data)):
+# for i in range(1): #range(len(symbol_data)):
+    total += place_all(symbol_data[i], number_data[i])
 
 print("P1: ", total)
