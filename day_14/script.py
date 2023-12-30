5cb9349036b7c24ce461f654ed14200f266bef7e
mmmbvl <mmmbvl@gmail.com>
2023-12-29 18:45:47 -0800

    D14P2 attempt

--- day_14/script.py
+++ day_14/script.py
@@ -2,11 +2,7 @@ inp = open("input").readlines()
 
 platform = [list(x.split("\n")[0]) for x in inp]
 
-def roll_rocks(platform):
-    # platform = platform[:]
-    pf = []
-    for p in platform:
-        pf.append(p[:])
+def roll_rocks(pf):
     for c in range(len(pf[0])):
         last_empty = 0
         for r in range(len(pf)):
@@ -26,6 +22,19 @@ def roll_rocks(platform):
                     last_empty = next_empty
     return pf
 
+def rotate_platform(platform, dir):
+    pf = []
+    if dir == "N":
+        for r in range(len(platform)):
+            pf.append(platform[r][:]) 
+    if dir == "W":
+        for c in range(len(platform[0])):
+            new_row = []
+            for r in range(len(platform)):
+                new_row.append(platform[(len(platform) - 1) -r][c])
+            pf.append(new_row)
+    return pf
+
 def weigh(platform):
     tot = 0
     for r in range(len(platform)):
@@ -33,6 +42,16 @@ def weigh(platform):
             if platform[r][c] == "O": tot += len(platform) - r
     return tot
 
+def cycle(platform):
+    weighs = []
+
+    for d in ["N", "W", "W", "W"]:
+        platform = rotate_platform(platform, d)
+        platform = roll_rocks(platform)
+        weighs.append(weigh(platform))
+        print(weigh(platform))
+    platform = rotate_platform(platform, "W")
+    return [platform, weighs]
 # print("\n\nOriginal Platform")
 # for p in platform:
 #     print(p[0:25])
@@ -44,5 +63,41 @@ def weigh(platform):
 def solve(platform, part):
     if part == "P1":
         print("P1: ", weigh(roll_rocks(platform)))
+    if part == "P2":
+        previous_weights = [0]
+        cyc = 0
+
+        for i in range(1000000000):
+            nyr = cycle(platform)
+            platform = nyr[0]
+            previous_weights += nyr[1]
+            # print(i, previous_weights[-1])
+
+            is_steady_state = False
+            for cycle_length in range(1, len(previous_weights)):
+                is_cycle = True
+                for j in range(len(previous_weights) - 1, len(previous_weights) - 1 - cycle_length,-1):
+                    if previous_weights[j] != previous_weights[j - cycle_length]:
+                        is_cycle = False
+                        break
+
+                if is_cycle and i > 1000:
+                    print("Cycle found with length: ", cycle_length)
+                    is_steady_state = True
+                    cyc = cycle_length
+                    break
+            if is_steady_state:
+                break
+        
+        weight_at_billion = 0
+        for k in range(len(previous_weights) - 1, 0, -1):
+            if (1000000000 - k) % cyc == 0:
+                weight_at_billion = previous_weights[k]
+                break
+        print("P2: ", weight_at_billion)
 
 solve(platform, "P1")
+solve(platform, "P2")
+
+# 142839 too high
+# 106576 too high
