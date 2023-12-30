393f6ad5a594cb596841433c8642284264306e7f
mmmbvl <mmmbvl@gmail.com>
2023-12-29 16:26:32 -0800

    D13P2 solution

--- day_13/script.py
+++ day_13/script.py
@@ -14,66 +14,39 @@ def print_patterns(ps, num):
             for j, n in enumerate(m):
                 print(n)
 
-# print_patterns(patterns)
-
-def find_vert_LOR(p, dbg):
-    t = []
-    if dbg:
-        print("Examining vertical lines of reflection")
-    for r in p:
-        if dbg:
-            print("\n", r)
-            dbgArr = ["_" for i in range(len(r) - 1)]
-        for bp in range(1, len(r)):
-            if dbg:
-                dbgArr = ["_" for i in range(len(r) - 1)]
-            if dbg:
-                print(r[0:bp], list((r[bp:])))
-            # isReflective = p[0:bp] == list(reversed(p[bp:]))
-            isReflective = True
-            for h in range(bp):
-                if (bp - h) + bp - 1< len(r) and r[h] != r[(bp - h) + bp - 1]:
-                    if dbg:
-                        print("hit falsity: ", h, r[h], r[(bp - h + bp) - 1])
-                    isReflective = False
-            if dbg:
-                print(isReflective, bp)
-
-            if (isReflective):
-                if not bp in t:
-                    t.append(bp)
-            else:
-                if bp in t:
-                    t.remove(bp)
-            if dbg:
-                dbgArr.insert(bp,"^")
-            if dbg:
-                print(dbgArr)
-    return t
-
 def find_hori_LOR(p, dbg):
     lor = 0
-    xt = []
+    mistakes = {}
     possible_lors = range(1,len(p))
 
     for p_lor in possible_lors:
         bi = p_lor - 1
         ui = p_lor
         isReflective = True
-        while bi >= 0 and ui < len(p):
 
-            if "".join(p[bi]) != "".join(p[ui]):
-                isReflective = False
+        ms = 0
+        while bi >= 0 and ui < len(p):
+            for a in range(len(p[0])):
+                if p[bi][a] != p[ui][a]:
+                    ms += 1
+                    isReflective = False
             bi -= 1
             ui += 1
+
         if dbg:
             print("\n\n")
             for qw in range(len(p)):
                 if qw == p_lor: print("--------------------------", isReflective, p_lor)
                 print(p[qw])        
         if isReflective: lor = p_lor
-        if isReflective: xt.append(p_lor)
-    return lor
+
+        mistakes[p_lor] = ms
+
+    for i in mistakes:
+        if mistakes[i] == 1:
+            return i
+    if dbg: print(mistakes, "outcome")
+    return 0
 
 def transp(mtx):
     new_mtx = []
@@ -94,7 +67,7 @@ def transp(mtx):
     return new_mtx
 
 
-g = 18
+g = 42
 print_patterns(patterns, g)
 print("\n")
 gm = transp(patterns[g - 1])
