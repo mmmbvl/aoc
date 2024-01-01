860ca8f4d9887d385a03194e307beb919d449373
mmmbvl <mmmbvl@gmail.com>
2023-12-31 22:06:08 -0800

    D17P2 solution

--- day_17/script.py
+++ day_17/script.py
@@ -3,9 +3,6 @@ inp = open("input").readlines()
 from heapq import heappop, heappush
 from typing import NamedTuple
 
-min_straight_moves = 0
-max_straight_moves = 3
-
 directions = ["N", "W", "S", "E"]
 directions_change = {
     0: [-1, 0],
@@ -33,35 +30,40 @@ class Coordinates(NamedTuple):
 endpoint = Coordinates(len(city) - 1, len(city[0]) - 1, 0)
 
 State = tuple[int, Coordinates, int]
-def solve(part):
-    if part == "P1":
-        queue: list[State] = [
-            (0, Coordinates(0, 0, 2), 0),
-            (0, Coordinates(0, 0, 3), 0)
-        ]
-        seen: set[tuple[Coordinates, int]] = set()
+def solve(part, mn, mx):
+    min_straight_moves = mn
+    max_straight_moves = mx
+    # if part == "P1":
+    queue: list[State] = [
+        (0, Coordinates(0, 0, 2), 0),
+        (0, Coordinates(0, 0, 3), 0)
+    ]
+    seen: set[tuple[Coordinates, int]] = set()
 
-        while queue:
-            cost, pos, num_steps = heappop(queue)
-            if pos.r == endpoint.r and pos.c == endpoint.c and num_steps >= min_straight_moves:
-                return cost
+    while queue:
+        cost, pos, num_steps = heappop(queue)
+        if pos.r == endpoint.r and pos.c == endpoint.c and num_steps >= min_straight_moves:
+            return cost
 
-            if (pos, num_steps) in seen:
-                continue
-            seen.add((pos, num_steps))
-            left_pt = pos.turn_CCW().move_fwd()
-            right_pt = pos.turn_CW().move_fwd()
-            fwd_pt = pos.move_fwd()
+        if (pos, num_steps) in seen:
+            continue
+        seen.add((pos, num_steps))
+        left_pt = pos.turn_CCW().move_fwd()
+        right_pt = pos.turn_CW().move_fwd()
+        fwd_pt = pos.move_fwd()
 
-            if (num_steps >= min_straight_moves and left_pt.r >= 0 and left_pt.r < len(city) and left_pt.c >= 0 and left_pt.c < len(city[0])):
-                heappush(queue, (cost + int(city[left_pt.r][left_pt.c]), left_pt, 1))
+        if (num_steps >= min_straight_moves and left_pt.r >= 0 and left_pt.r < len(city) and left_pt.c >= 0 and left_pt.c < len(city[0])):
+            heappush(queue, (cost + int(city[left_pt.r][left_pt.c]), left_pt, 1))
 
-            if (num_steps >= min_straight_moves and right_pt.r >= 0 and right_pt.r < len(city) and right_pt.c >= 0 and right_pt.c < len(city[0])):
-                heappush(queue, (cost + int(city[right_pt.r][right_pt.c]), right_pt, 1))
+        if (num_steps >= min_straight_moves and right_pt.r >= 0 and right_pt.r < len(city) and right_pt.c >= 0 and right_pt.c < len(city[0])):
+            heappush(queue, (cost + int(city[right_pt.r][right_pt.c]), right_pt, 1))
 
-            if (num_steps < max_straight_moves and fwd_pt.r >= 0 and fwd_pt.r < len(city) and fwd_pt.c >= 0 and fwd_pt.c < len(city[0])):
-                heappush(queue, (cost + int(city[fwd_pt.r][fwd_pt.c]), fwd_pt, num_steps + 1))
+        if (num_steps < max_straight_moves and fwd_pt.r >= 0 and fwd_pt.r < len(city) and fwd_pt.c >= 0 and fwd_pt.c < len(city[0])):
+            heappush(queue, (cost + int(city[fwd_pt.r][fwd_pt.c]), fwd_pt, num_steps + 1))
 
-        return -1
+    return -1
+    # if part == "P2":
+    #     return solve("P1")
 
-print("P1: ", solve("P1"))
+print("P1: ", solve("P1", 0, 3))
+print("P2: ", solve("P2", 4, 10))
