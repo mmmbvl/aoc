9e238904f9fac84a4af6a806144b8d348df69d6a
mmmbvl <mmmbvl@gmail.com>
2024-01-01 16:36:53 -0800

    D18P1 solution, not robust

--- day_18/script.py
+++ day_18/script.py
@@ -37,26 +37,80 @@ for r in range(size[0] + size[2]):
 
 curr_pos = [size[0], size[1]]
 
-def display(mv, curr_pos):
+def display(mv, curr_pos, grid):
     limit_row = 50
     limit_col = 150
 
-    a = int(curr_pos[0] - (limit_row/2))
-    b = int(curr_pos[0] + (limit_row/2))
-    c = int(curr_pos[1] - (limit_col/2))
-    d = int(curr_pos[1] + (limit_col/2))
+    a = int(curr_pos[0] - (limit_row/2)) if int(curr_pos[0] - (limit_row/2)) >= 0 else 0
+    b = int(curr_pos[0] + (limit_row/2)) if int(curr_pos[0] + (limit_row/2)) < len(grid) else len(grid) - 1
+    c = int(curr_pos[1] - (limit_col/2)) if int(curr_pos[0] - (limit_col/2)) >= 0 else 0
+    d = c + limit_col if c + limit_col < len(grid[0]) else len(grid[0]) - 1
+    # d = int(curr_pos[1] + (limit_col/2)) if int(curr_pos[0] + (limit_col/2)) < len(grid[0]) else len(grid[0]) - 1
 
     os.system("clear")
     print("Current grid")
     print("Move: ", mv)
+    print(a, b, c, d)
     for row in grid[a:b]:
         print("".join(row[c:d]))
-    time.sleep(0.05)
+    time.sleep(0.01)
 
 for inst in instructions:
     move_ch = directions[inst[0]]
-    display(inst, curr_pos)
+    display(inst, curr_pos, grid)
     for i in range(inst[1]):
         grid[curr_pos[0]][curr_pos[1]] = "#"
         curr_pos[0] += move_ch[0]
         curr_pos[1] += move_ch[1]
+
+inside_pt = []
+outside_pt = [0,0]
+num_digged = 0
+# for r in range(len(grid)):
+#     is_inside = False
+#     for c in range(len(grid[0])):
+#         if r % 50 == 0 and r > 600:
+#             if c % 100 == 0:
+#                 display(instructions[-1], [r,c], grid)
+#                 time.sleep(1)
+#         if grid[r][c] == "#" and is_inside == False:
+#             is_inside = True
+#             num_digged += 1
+#         elif grid[r][c] == "#" and is_inside == True:
+#             num_digged += 1
+#             is_inside = False
+#         elif is_inside == True:
+#             grid[r][c] = "#"
+#             num_digged += 1
+#         else:
+#             continue
+
+def flood(pt, ch):
+    arr = []
+    arr.append(pt)
+
+    while arr:
+        px = arr.pop(0)
+        grid[px[0]][px[1]] = ch
+        print(px)
+
+        if px[0] % 25 == 0 and px[0] > 500:
+            if px[1] % 25 == 0 and px[1] > 500:
+                display(instructions[-1], px, grid)
+                # time.sleep(1)
+
+        for d in directions:
+            new_pt = [px[0] + directions[d][0], px[1] + directions[d][1]]
+            if new_pt[0] >= 0 and new_pt[1] >= 0 and new_pt[0] < len(grid) and new_pt[1] < len(grid[0]) and grid[new_pt[0]][new_pt[1]] == ".":
+                grid[new_pt[0]][new_pt[1]] = ch
+                arr.append(new_pt)
+
+flood(outside_pt, "G")
+ng = 0
+for r in range(len(grid)):
+    for c in range(len(grid[0])):
+        if grid[r][c] == "G": ng += 1
+
+
+print("P1: ", (len(grid) * len(grid[0])) - ng)
+# 111329 too high
