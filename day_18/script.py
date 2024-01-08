c95ce987366180adc1f3063d11605cb30f09a02a
mmmbvl <mmmbvl@gmail.com>
2024-01-07 21:55:03 -0800

    D18P2 partial....

--- day_18/script.py
+++ day_18/script.py
@@ -11,6 +11,13 @@ def process_instructions(inp):
         instructions.append((p[0], int(p[1]), p[2]))
     return instructions
 
+opposite_directions = {
+    "U": "D",
+    "D": "U",
+    "L": "R",
+    "R": "L"
+}
+
 directions = {
     "U": [-1, 0],
     "D": [ 1, 0],
@@ -95,6 +102,130 @@ def count_dug(grid):
             if grid[r][c] == "#": num_dug += 1
     return num_dug
 
+def make_new_instructions(inst):
+    # inst (dir, length, #color)
+    new_inst = []
+    curr_pos = [0, 0]
+    new_inst.append(["U", 0, 0, 0])
+    for x in inst:
+        color = x[2][1:]
+        new_dir = color[-1]
+        if (new_dir == "0"):
+            new_dir = "R"
+        if (new_dir == "1"):
+            new_dir = "D"
+        if (new_dir == "2"):
+            new_dir = "L"
+        if (new_dir == "3"):
+            new_dir = "U"
+        new_length = color[:-1]
+
+        move_ch = directions[x[0]]
+        curr_pos[0] += move_ch[0] * x[1]
+        curr_pos[1] += move_ch[1] * x[1]
+        print("Turning old inst: ", x, " into new instruction: ", (new_dir, new_length, curr_pos[0], curr_pos[1]))
+        new_inst.append((new_dir, new_length, curr_pos[0], curr_pos[1]))
+    return new_inst
+
+def reduce_collinear_inst(in1, in2):
+    new_dir = "X"
+    new_len = "X"
+
+    dir1 = in1[0]
+    dir2 = in2[0]
+
+    if dir1 == dir2:
+        new_dir = dir1
+        new_len = in1[1] + in2[1]
+        return ("success", (new_dir, new_len, in2[2], in2[3]))
+    
+    if dir2 == opposite_directions[dir1]:
+        if in1[1] > in2[1]:
+            new_dir = dir1
+            new_len = in1[1] - in2[1]
+            return ("success", (new_dir, new_len, in2[2], in2[3]))
+        if in2[1] > in1[1]:
+            new_dir = dir2
+            new_len = in2[1] - in1[1]
+            return ("success", (new_dir, new_len, in2[2], in2[3]))
+        return ("cancelled out", "")
+    
+    return ("not collinear", "")
+
+def reduce_boxlike_inst(in1, in2, in3):
+    # CW : positive; CCW: negative
+    if in3[0] == opposite_directions[in1[0]] and in2[0] != in1[0] and in2[0] != in3[0]:
+        if in1[0] == "U" or in1[0] == "D":
+            cwdir = 1
+            if in1[0] == "D":
+                cwdir *= -1
+            if in2[0] == "L":
+                cwdir *= -1
+            midpoint = [in2[2],in1[3]]
+            if midpoint[3] == in3[3]:
+                return ("success-c", cwdir * in2[1] * in1[1], (in2[0], in2[1], in3[2], in3[3]))
+            else:
+                new_dir = ""
+                new_len = 0
+                if in1[1] > in2[1]:
+                    new_dir = in1[0]
+                    new_len = in1[1] - in2[1]
+                if in2[1] > in1[1]:
+                    new_dir = in2[0]
+                    new_len = in2[1] - in1[1]
+                return ("success", cwdir * in2[1] * in1[1], (in2[0], in2[1], midpoint[2], midpoint[3]), (new_dir, new_len, in3[2], in3[3]))
+        if in1[0] == "R" or in1[0] == "L":
+            cwdir = 1
+            if in1[0] == "U":
+                cwdir *= -1
+            if in2[0] == "L":
+                cwdir *= -1
+            midpoint = [in1[2],in2[3]]
+            if midpoint[2] == in3[2]:
+                return ("success-c", cwdir * in2[1] * in1[1], (in2[0], in2[1], in3[2], in3[3]))
+            else:
+                new_dir = ""
+                new_len = 0
+                if in1[1] > in2[1]:
+                    new_dir = in1[0]
+                    new_len = in1[1] - in2[1]
+                if in2[1] > in1[1]:
+                    new_dir = in2[0]
+                    new_len = in2[1] - in1[1]
+                return ("success", cwdir * in2[1] * in1[1], (in2[0], in2[1], midpoint[2], midpoint[3]), (new_dir, new_len, in3[2], in3[3]))
+    else:
+        return ("not boxlike", 0, "")
+    
+def reduce_instructions(instructions):
+    area = 0
+    while instructions:
+        if len(instructions) == 2:
+            break
+        for i in range(1, len(instructions)):
+            p = reduce_collinear_inst(instructions[i - 1], instructions[i])
+            if p[0] == "success":
+                instructions = instructions[:i-1] + p[1] + instructions[i+1:]
+                break
+            if p[0] == "cancelled out":
+                instructions = instructions[:i-1] + instructions[i+1:]
+                break
+        for i in range(2, len(instructions)):
+            p = reduce_boxlike_inst(instructions[i - 2], instructions[i-1], instructions[i])
+            if p[0] == "success":
+                instructions = instructions[:i-2] + p[2] + p[3] + instructions[i+1:]
+                area += p[1]
+                break
+            if p[0] == "success-c":
+                instructions = instructions[:i-2] + p[2] + instructions[i+1:]
+                area += p[1]
+                break
+    return area
+
+
+
+
+
+
 def solve(part):
     if part == "P1":
         inst = process_instructions(inp)
@@ -107,51 +238,8 @@ def solve(part):
     if  part == "P2":
         inst = process_instructions(inp)
         new_inst = make_new_instructions(inst)
+        answer = reduce_instructions(new_inst)
+        print("P2: ", answer)
 
-solve("P1")
-
-def make_new_instructions(inst):
-    # inst (dir, length, (#color))
-    new_inst = []
-    for x in inst:
-        color = x[2][2:-1]
-        new_dir = color[-1]
-        if (new_dir == "0"):
-            new_dir = "R"
-        if (new_dir == "1"):
-            new_dir = "D"
-        if (new_dir == "2"):
-            new_dir = "L"
-        if (new_dir == "3"):
-            new_dir = "U"
-        new_length = color[:-1]
-        new_inst.append((new_dir, new_length, ""))
-    return new_inst
-
-def internal_area_dig(instructions):
-    rightwise_area = []
-    curr_pos = [0, 0]
-    rightwise_area.append(["X", 0, 0, 0])
-    for inst in instructions:
-        move_ch = directions[inst[0]]
-        curr_pos[0] += move_ch[0] * inst[1]
-        curr_pos[1] += move_ch[1] * inst[1]
-        rightwise_area.append([inst[0], inst[1], curr_pos[0], curr_pos[1]])
-        if len(rightwise_area) > 2:
-            previous_scnd_dir = rightwise_area[-3][0]
-            previous_dir = rightwise_area[-2][0]
-            curr_dir = rightwise_area[-1][0]
-
-        addable = curr_dir == previous_dir
-        if addable:
-            current_area_n = rightwise_area.pop()
-            previous_area_n = rightwise_area.pop()
-            rightwise_area.append([curr_dir, current_area_n[1] + previous_area_n[1], curr_pos[0], curr_pos[1]])
-
-        subtractable = (curr_dir == "U" and previous_dir == "D") or (curr_dir == "D" and previous_dir == "U") or (curr_dir == "R" and previous_dir == "L") or (curr_dir == "L" and previous_dir == "R")
-        if subtractable:
-            current_area_n = rightwise_area.pop()
-            previous_area_n = rightwise_area.pop()
-            rightwise_area.append([curr_dir, current_area_n[1] + previous_area_n[1], curr_pos[0], curr_pos[1]])
-
-
+# solve("P1")
+solve("P2")
